ccc9ebbe12cb2e8e09534f9c3f4de6f0ab07729e
Abeer Altaf <Abeer Altaf@DESKTOP-OJJNPQG>
2018-01-24 20:01:53 -0500

    Finished selenium testing for add functionality.

--- CoffeeMaker/src/test/java/edu/ncsu/csc/selenium/AddRecipeTest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/selenium/AddRecipeTest.java
@@ -61,7 +61,7 @@ public class AddRecipeTest extends SeleniumTest {
 
         // Enter the recipe information
         driver.findElement( By.name( "name" ) ).clear();
-        driver.findElement( By.name( "name" ) ).sendKeys( "Latte" );
+        driver.findElement( By.name( "name" ) ).sendKeys( "Coffee" );
         driver.findElement( By.name( "price" ) ).clear();
         driver.findElement( By.name( "price" ) ).sendKeys( "50" );
         driver.findElement( By.name( "coffee" ) ).clear();
@@ -85,7 +85,7 @@ public class AddRecipeTest extends SeleniumTest {
         driver.findElement( By.name( "name" ) ).clear();
         driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
         driver.findElement( By.name( "price" ) ).clear();
-        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "price" ) ).sendKeys( "-50" );
         driver.findElement( By.name( "coffee" ) ).clear();
         driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
         driver.findElement( By.name( "milk" ) ).clear();
@@ -93,7 +93,7 @@ public class AddRecipeTest extends SeleniumTest {
         driver.findElement( By.name( "sugar" ) ).clear();
         driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
         driver.findElement( By.name( "chocolate" ) ).clear();
-        driver.findElement( By.name( "chocolate" ) ).sendKeys( "0" );
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
 
         // Submit the recipe.
         driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
@@ -105,7 +105,73 @@ public class AddRecipeTest extends SeleniumTest {
 
         // Enter the recipe information
         driver.findElement( By.name( "name" ) ).clear();
-        driver.findElement( By.name( "name" ) ).sendKeys( "Iced coffee" );
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "-3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper5 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "-1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper6 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "-1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper7 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
         driver.findElement( By.name( "price" ) ).clear();
         driver.findElement( By.name( "price" ) ).sendKeys( "50" );
         driver.findElement( By.name( "coffee" ) ).clear();
@@ -115,7 +181,183 @@ public class AddRecipeTest extends SeleniumTest {
         driver.findElement( By.name( "sugar" ) ).clear();
         driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
         driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "-2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper8 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "a" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper9 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "a" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper10 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "a" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper11 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "a" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper12 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper13 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Mocha" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "50" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper14 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Latte" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "60" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "3" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "2" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
         driver.findElement( By.name( "chocolate" ) ).sendKeys( "0" );
+
+        // Submit the recipe.
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+    }
+
+    private void addRecipeHelper15 () {
+        driver.get( baseUrl );
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        // Enter the recipe information
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( "Hot Chocolate" );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "75" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "0" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "2" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "2" );
 
         // Submit the recipe.
         driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
@@ -144,24 +386,181 @@ public class AddRecipeTest extends SeleniumTest {
      */
     @Test
     public void testAddRecipe2 () throws Exception {
-        addRecipeHelper();
+        addRecipeHelper2();
 
         assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
     }
 
     /**
-     * addRecipe4 Test for adding a fourth recipe. Expect to get an appropriate
-     * error message.
-     * 
+     * addRecipe3 Test for adding a negative value for price. Expect to get an
+     * appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe3 () throws Exception {
+        addRecipeHelper3();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe4 Test for adding a negative value for coffee. Expect to get an
+     * appropriate error message.
+     *
      * @throws Exception
      */
     @Test
     public void testAddRecipe4 () throws Exception {
-        addRecipeHelper2();
-        addRecipeHelper3();
+        addRecipeHelper4();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe5 Test for adding a negative value for milk. Expect to get an
+     * appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe5 () throws Exception {
+        addRecipeHelper5();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe6 Test for adding a negative value for sugar. Expect to get an
+     * appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe6 () throws Exception {
+        addRecipeHelper6();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe7 Test for adding a negative value for chocolate. Expect to get
+     * an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe7 () throws Exception {
+        addRecipeHelper7();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe8 Test for adding a non-numeric value for price. Expect to get
+     * an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe8 () throws Exception {
+        addRecipeHelper8();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe9 Test for adding a non-numeric value for coffee. Expect to get
+     * an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe9 () throws Exception {
+        addRecipeHelper9();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe10 Test for adding a non-numeric value for milk. Expect to get
+     * an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe10 () throws Exception {
+        addRecipeHelper10();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe11 Test for adding a non-numeric value for sugar. Expect to get
+     * an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe11 () throws Exception {
+        addRecipeHelper11();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * addRecipe12 Test for adding a non-numeric value for chocolate. Expect to
+     * get an appropriate error message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe12 () throws Exception {
+        addRecipeHelper12();
+
+        assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
+    }
+
+    /**
+     * Test for a adding a recipe. Expect to get an appropriate success message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe13 () throws Exception {
+        addRecipeHelper13();
+
+        // Make sure the proper message was displayed.
         assertTrue( driver.getPageSource().contains( "Recipe Created" ) );
 
-        addRecipeHelper4();
+        System.out.println( driver.getPageSource() );
+    }
+
+    /**
+     * Test for a adding a recipe. Expect to get an appropriate success message.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe14 () throws Exception {
+        addRecipeHelper14();
+
+        // Make sure the proper message was displayed.
+        assertTrue( driver.getPageSource().contains( "Recipe Created" ) );
+
+        System.out.println( driver.getPageSource() );
+    }
+
+    /**
+     * addRecipe15 Test for adding a fourth recipe. Expect to get an appropriate
+     * error message
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddRecipe15 () throws Exception {
+        addRecipeHelper15();
+
         assertTrue( driver.getPageSource().contains( "Error while adding recipe." ) );
     }
 
